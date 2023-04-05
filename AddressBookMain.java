ac4238b8379dad8a114425ecf145854f7b511bb0
prajwalgunjal <prajwalgunjal8@gmail.com>
2023-04-05 11:26:42 +0530

    UC5:Added

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -6,10 +6,10 @@ public class AddressBookMain {
         Scanner sc = new Scanner(System.in);
         while(true){
             System.out.println("Welcome to AddressBook program");
-            System.out.println("1) Press 1 to add new contact");
-            System.out.println("2) press 2 to display contact");
+            System.out.println("1) Press 1 to Add new contact");
+            System.out.println("2) press 2 to Display contact");
             System.out.println("3) press 3 to Edit contact");
-            System.out.println("4) press 4 to delete contact");
+            System.out.println("4) press 4 to Delete contact");
             System.out.println("5) press 5 to Exit");
             int input = sc.nextInt();
             switch (input){
