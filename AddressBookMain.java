9e0e4d89a398f9d6bea5fe0cc47e3206c7316a5e
prajwalgunjal <prajwalgunjal8@gmail.com>
2023-04-05 11:24:35 +0530

    UC3:Added

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -8,6 +8,9 @@ public class AddressBookMain {
             System.out.println("Welcome to AddressBook program");
             System.out.println("1) Press 1 to add new contact");
             System.out.println("2) press 2 to display contact");
+            System.out.println("3) press 3 to Edit contact");
+            System.out.println("4) press 4 to delete contact");
+            System.out.println("5) press 5 to Exit");
             int input = sc.nextInt();
             switch (input){
                 case 1 -> {
@@ -16,7 +19,16 @@ public class AddressBookMain {
                 case 2 -> {
                     addressBook.display();
                 }
-
+                case 3 -> {
+                    addressBook.Edit();
+                }
+                case 5 ->{
+                    System.out.println("Thank you so much !!!");
+                    System.exit(0);
+                }
+                case 4 -> {
+                    addressBook.delete();
+                }
             }
         }
     }
