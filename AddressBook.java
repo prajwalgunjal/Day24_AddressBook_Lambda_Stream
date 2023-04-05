ee3dd6d0299255830de80a4998fbbdb67fe5b754
prajwalgunjal <prajwalgunjal8@gmail.com>
2023-04-05 11:21:29 +0530

    UC2 Added

--- AddressBook.java
+++ AddressBook.java
@@ -1,7 +1,45 @@
 package com.bridgelabz.Lambda;
-
 import java.util.ArrayList;
 import java.util.Scanner;
-
 public class AddressBook {
+
+    ArrayList<Contact> contactBook = new ArrayList<>();
+    public void AddContact(){
+        Scanner sc = new Scanner(System.in);
+        Contact contact = new Contact();
+        System.out.print("Enter First name :");
+        String firstname = sc.next();
+        contact.setFirstName(firstname);
+        System.out.print("Enter last name :");
+        String lastname = sc.next();
+        contact.setLastName(lastname);
+        System.out.print("Enter Address :");
+        String address = sc.next();
+        contact.setAddress(address);
+        System.out.print("Enter City :");
+        String city = sc.next();
+        contact.setCity(city);
+        System.out.print("Enter State :");
+        String state = sc.next();
+        contact.setState(state);
+        System.out.print("Enter zip code :");
+        String zip = sc.next();
+        contact.setZip(zip);
+        System.out.print("Enter phone number : ");
+        String phone = sc.next();
+        contact.setPhoneNumber(phone);
+        System.out.print("Enter Email :");
+        String email = sc.next();
+        contact.setEmail(email);
+        contactBook.add(contact);
+        System.out.println("Contact added Successfully!!!");
+    }
+    public void display(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter name of the contact : ");
+        String name = sc.next();
+        for(int i=0;i<contactBook.size();i++){
+            System.out.println(contactBook.get(i));
+        }
+    }
 }
--- AddressBookMain.java
+++ AddressBookMain.java
@@ -1,3 +1,23 @@
 package com.bridgelabz.Lambda;
+import java.util.Scanner;
 public class AddressBookMain {
+    public static void main(String[] args) {
+        AddressBook addressBook = new AddressBook();
+        Scanner sc = new Scanner(System.in);
+        while(true){
+            System.out.println("Welcome to AddressBook program");
+            System.out.println("1) Press 1 to add new contact");
+            System.out.println("2) press 2 to display contact");
+            int input = sc.nextInt();
+            switch (input){
+                case 1 -> {
+                    addressBook.AddContact();
+                }
+                case 2 -> {
+                    addressBook.display();
+                }
+
+            }
+        }
+    }
 }
